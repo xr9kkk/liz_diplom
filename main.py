f357c8d562b8c3c4ebf9c1e3cdb9d0f2d4697eec
POP LOLOVSKIY <ivankozlovvrn2005@gmail.com>
2024-11-23 01:48:15 +0300

    неплохо работает

--- main.py
+++ main.py
@@ -28,18 +28,20 @@ def plot_3d_route(route, warehouse):
 
     ax.set_title("Warehouse Route (3D View)")
     ax.set_xlabel("Columns (A-M)")
+    ax.set_xticks(range(1, 13))
     ax.set_ylabel("Rows (1-13)")
+    ax.set_yticks(range(1,13))
     ax.set_zlabel("Levels")
+    ax.set_zticks(range(1,3))
     ax.legend()
     plt.show()
-
 def generate_warehouse_map():
     warehouse = {}
     for row in range(1, 14):  
         for col in string.ascii_uppercase[:13]:  
             for level in range(1, 4):  
                 cell = f"{col}-{row}-{level}"
-                if row in [2, 3, 4] and col in "BCEFHIKL":
+                if row in [2, 3, 4, 6, 7, 8, 10, 11, 12] and col in "BCEFHIKL":
                     warehouse[cell] = "shelf"
                 else:
                     warehouse[cell] = "free"
@@ -72,10 +74,11 @@ def calculate_distances(points, warehouse):
                 distances[(start, end)] = len(path) if path else float('inf')
     return distances
 
+
 def is_valid_move(current, next_point, warehouse, is_on_second_level=False):
     if next_point not in warehouse or warehouse[next_point] == "shelf":
-        return False  # Запрещаем движение на недоступные ячейки или полки
-
+        return False  
+    
     cur_col, cur_row, cur_level = current.split('-')
     next_col, next_row, next_level = next_point.split('-')
     cur_row, next_row = int(cur_row), int(next_row)
